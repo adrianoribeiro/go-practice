2729e17870d19832bb5267fe4c7a575e65d1bfef
Adriano Ribeiro <adrianomsg@gmail.com>
2020-08-17 22:47:51 +0100

    Day 14 - Exercises from book Building Microservices with Go
    - Chapter 1 - Static file handler

---
+++ building-microservices-with-go/reading_writing_json_1/reading_writing_json_5.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type helloWorldResponse struct {
+	Message string `json:"message"`
+}
+
+type helloWorldRequest struct {
+	Name string `json:"name"`
+}
+
+func main() {
+	port := 8080
+
+	cathandler := http.FileServer(http.Dir("./images"))
+	http.Handle("/cat/", http.StripPrefix("/cat/", cathandler))
+
+	log.Printf("Server starting on port %v\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+}
